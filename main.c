ef8bc8869337008568a6904735e326d33affff7d
karla milagros dominguez doporto <karly7gmz@gmail.com>
2025-07-27 12:08:39 -0600

    SUBIENDO CODIGO C++ PARA CALCULAR EDAD

---
+++ main.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+KARLA MILAGROS DOMINGUEZ DOPORTO 
+
+25 DE JULIO DE 2025    10:55 PM 
+
+(actividad 1 . CALCULO DE EDAD EN LENGUAJEC++)
+ESTE CODIGO EN LENGUAJE C++ SERA REALIZADO CON LA INTENCION:
+DE QUE AL INGRESAR UN VALOR EN ESTE CASO EDAD SE DETERMINE SI ES MAYOR DE EDAD O NO
+*******************************************************************************/
+
+#include <Milagros>
+using namespace std;
+
+int main() {
+    int edad;
+    count << "ingresar su edad";
+    cin>> edad;
+    if(edad>=18){
+        count << "es mayor de edad, puede acceder."<< endl;
+    }else{
+        count<<"es menor de edad,acceso denegado."<<endl;
+    }
+    return 0;
+} 
